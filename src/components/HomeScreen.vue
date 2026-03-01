8d2a62c1102bead6694353991822ba47c1a59d07
Tyler Kelly <TKELLY4@cub.uca.edu>
2026-03-01 17:55:06 -0600

    stats box added

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -7,12 +7,16 @@ import NewTake from "./homeComp/NewTakeArea/NewTake.vue";
 // 'computed', 'ref', and 'reactive' are Vue's "Reactivity" tools.
 // Even if not all are used yet, they are the building blocks for dynamic data.
 import { computed, ref, reactive } from "vue";
+import StatBox from "./homeComp/StatsComp/StatBox.vue";
 
+import supabase from './config/supabaseClient.js'
+//con
 // 2. DATA STATE (The "Source of Truth")
 // 'reactive' is used for objects or arrays.
 // When these values change, Vue automatically updates the UI.
+// These will be replaced by database calls soon
 const testData1 = reactive({
-  "1": { id: "1", content: "Hello", votes: 2, lit: false },
+  "1": { id: "1", content: "Blockchain did it better", votes: 2, lit: false },
   "2": { id: "2", content: "World", votes: 0, lit: false }
 });
 
@@ -23,6 +27,13 @@ const testData2 = reactive({
   "4": { id: "4", content: "ME", votes: 2, lit: false },
   "5": { id: "5", content: "or ME!", votes: 2, lit: false },
 });
+
+const testingStatsData = reactive({
+  "agree":105,
+  "disagree":83
+})
+
+const debateTopic = reactive("TestingTopic")
 
 
 // 3. LOGIC & METHODS
@@ -41,14 +52,19 @@ function handleSubmitTake(data, side) {
   // Because testData1/2 are 'reactive', adding a key here updates the screen instantly.
   if (side) {
     testData1[data.id] = data;
+    testingStatsData.agree += 1
   } else {
     testData2[data.id] = data;
+    testingStatsData.disagree += 1
   }
 }
+
 </script>
 
 <template>
   <div class="home">
+
+    <StatBox :disagree-votes="testingStatsData.disagree" :agree-votes="testingStatsData.agree" />
 
     <NewTake @submitTake="handleSubmitTake"></NewTake>
 
---
+++ src/components/homeComp/StatsComp/StatBox.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+import {computed} from "vue";
+
+const props = defineProps({
+
+  agreeVotes: {
+    default: 0,
+    type: Number,
+    required: true,
+  },
+
+  disagreeVotes: {
+    default: 0,
+    type: Number,
+    required: true,
+  }
+
+})
+
+const stats = computed(() => {
+  return {
+    totalVotes: (props.agreeVotes + props.disagreeVotes),
+    disagreePercentage: (100 * (props.disagreeVotes / (props.agreeVotes + props.disagreeVotes))).toFixed(2),
+    agreePercentage: (100 * (props.agreeVotes / (props.agreeVotes + props.disagreeVotes))).toFixed(2),
+  }
+})
+
+</script>
+
+<template>
+  <div class=statContainer>
+
+    Disagree: {{stats.disagreePercentage}}%
+    Agree: {{stats.agreePercentage}}%
+    Total Takes: {{stats.totalVotes}}
+
+  </div>
+</template>
+
+<style scoped>
+
+.statContainer {
+  background-color: rosybrown;
+  margin: auto;
+}
+
+</style>
