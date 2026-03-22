67fb7c95d9ab2bb3a842b8bda9295fd60469970d
Tyler <tyler.sh.kelly@gmail.com>
2026-03-21 21:54:29 -0500

    moved env vars

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ngukguvinzmbnnhngctr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndWtndXZpbnptYm5uaG5nY3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0ODY0MDQsImV4cCI6MjA4NjA2MjQwNH0.g6qfGwOxKfJPXJdl4vCF2T0qoeE4ougbcich6xk5uM8'
+// Vite automatically injects these from your .env file
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
