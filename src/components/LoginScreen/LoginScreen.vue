53a23fb69e46a3c4f42246eff5afbced5a6b44d3
Tyler Kelly <tyler.sh.kelly@gmail.com>
2026-04-16 16:02:52 -0500

    re-organized login screen

--- src/components/LoginScreen/LoginScreen.vue
+++ src/components/LoginScreen/LoginScreen.vue
@@ -82,6 +82,9 @@
 
 <template>
 
+
+
+
   <div class="bg-background text-on-background flex flex-col min-h-screen items-center justify-center p-6 selection:bg-primary selection:text-white">
 
     <!-- Conditonal render if there was an error in logging in -->
@@ -103,7 +106,52 @@
   <!-- Main Authentication Block -->
     <div class="bg-white border-[2px] border-black p-8 md:p-12 shadow-brutal">
 
-        <!-- ID Input Group -->
+
+      <h3 class="text-3xl md:text-8xl font-black tracking-tighter uppercase leading-[0.85] mb-4">
+        ONE TAP LOGIN
+      </h3>
+
+      <button
+          @click="handleGoogleLogin"
+          type="button"
+          class="group relative w-full flex items-center justify-center gap-4 bg-white border-4 border-black p-4 transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-brutal active:translate-x-0 active:translate-y-0 active:shadow-none"
+      >
+
+        Continue With Google
+        <svg class="w-6 h-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
+          <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
+          <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z" fill="#FBBC05"/>
+          <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
+        </svg>
+
+        <span class="text-xl font-black uppercase tracking-widest">
+    </span>
+
+      </button>
+
+      <p class="text-center text-[9px] font-bold uppercase tracking-widest opacity-50">
+        Encrypted Handshake via Google Identity Services
+      </p>
+
+
+      <!-- ID Input Group -->
+
+
+      <div class="mt-8 space-y-6">
+        <div class="relative flex items-center">
+          <div class="flex-grow border-t-2 border-black"></div>
+          <span class="flex-shrink mx-4 text-[10px] font-black tracking-[0.2em] uppercase text-black">
+      OR // MANUAL LOGIN
+    </span>
+          <div class="flex-grow border-t-2 border-black"></div>
+        </div>
+
+
+
+      </div>
+
+
 
       <form @submit.prevent="handleLogin">
         <div class="space-y-2">
@@ -130,39 +178,6 @@
         Create an Account
       </button>
 
-    </div>
-
-    <div class="mt-8 space-y-6">
-      <div class="relative flex items-center">
-        <div class="flex-grow border-t-2 border-black"></div>
-        <span class="flex-shrink mx-4 text-[10px] font-black tracking-[0.2em] uppercase text-black">
-      OR // EXTERNAL_PROVIDER
-    </span>
-        <div class="flex-grow border-t-2 border-black"></div>
-      </div>
-
-      <button
-          @click="handleGoogleLogin"
-          type="button"
-          class="group relative w-full flex items-center justify-center gap-4 bg-white border-4 border-black p-4 transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-brutal active:translate-x-0 active:translate-y-0 active:shadow-none"
-      >
-
-        Continue With Google
-        <svg class="w-6 h-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
-          <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
-          <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z" fill="#FBBC05"/>
-          <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
-        </svg>
-
-        <span class="text-xl font-black uppercase tracking-widest">
-    </span>
-
-      </button>
-
-      <p class="text-center text-[9px] font-bold uppercase tracking-widest opacity-50">
-        Encrypted Handshake via Google Identity Services
-      </p>
     </div>
 
 
